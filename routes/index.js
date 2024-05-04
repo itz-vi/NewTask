3a78010980d9187bdfe7eeaf58196dc791f24f2b
Vishal Malviya <159767904+itz-vi@users.noreply.github.com>
2024-05-04 08:39:03 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,42 @@ const express = require('express');
 const router = express.Router();
 const taskModel = require("./tasks");
 
-
 router.get("/", async function (req, res) {
   const tasks = await taskModel.find();
   res.render("addtask", { tasks })
 });
 
-
-router.post('/addtask', async function (req, res, next) {
+router.post('/addtask', async function (req, res) {
   const { task, status, deadline } = req.body;
   const newtask = await taskModel.create({ task, status, deadline })
-
   const tasks = await taskModel.find();
-
   res.render('addtask', { tasks })
 });
+
+
+
+router.get("/update/:id", async function (req, res) {
+  const id = req.params.id;
+  const task = await taskModel.findOne({ _id: id });
+  res.render("update", { task })
+})
+
+router.post("/updatetask/:id", async function (req, res) {
+  const { task, status, deadline } = req.body;
+  const id = req.params.id;
+  await taskModel.findByIdAndUpdate(id, { task, status, deadline }, { new: true });
+  const tasks = await taskModel.find();
+  res.render("addtask", { tasks })
+})
+
+
+
+
+router.get("/delete/:id", async function (req, res) {
+  const id = req.params.id;
+  await taskModel.deleteOne({ _id: id });
+  const tasks = await taskModel.find();
+  res.render("addtask", { tasks })
+})
 
 module.exports = router;
